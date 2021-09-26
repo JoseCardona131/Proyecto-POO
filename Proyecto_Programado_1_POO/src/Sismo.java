2a05b48654c65e7b08cd63a27c185841160d2163
JoseCardona131 <jcardona2003@outlook.com>
2021-09-25 21:37:25 -0600

    Primer Avance
    Formal

---
+++ Proyecto_Programado_1_POO/src/Sismo.java
@@ -0,0 +1,116 @@
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jcard
+ */
+public class Sismo {
+    
+    // Atributos
+    private String nombre;
+    
+    private GregorianCalendar fecha_hora;
+    
+    private double profundidad;
+    
+    private TOrigen tipoOrigen;
+    
+    private double magnitud;
+    
+    private String localizacion;
+    
+    private Provincia provincia;
+    
+    private boolean maritimo;
+    
+    private String descripcion;
+    
+    // Métodos
+    
+    // Constructor
+    public Sismo(){}
+    
+    // Setters/Getters
+    
+    //Nombre
+    public void setNombre(String name){
+        nombre = name;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    
+    // Fecha y hora
+    public void setFechaHora(int anio, int mes, int dia, int hora, int minuto){
+        fecha_hora.set(anio,mes,dia,hora,minuto);
+    }
+    
+    public GregorianCalendar getFechaHora(){
+        return fecha_hora;
+    }
+    
+    // Profundidad
+    public void setProfundidad(double profundity){
+        profundidad = profundity;
+    }
+    
+    public double getProfundidad(){
+        return profundidad;
+    }
+    
+    public void setTipoOrigen(TOrigen tipo){
+        tipoOrigen = tipo;
+    }
+    public  TOrigen get(){
+        return tipoOrigen;
+    }
+    public void setMagnitud(double mag){
+        magnitud = mag;
+    }
+    public double getMagnitud(){
+        return magnitud;
+    }
+    
+    public void setLocalizacion(String latitud_longitud){
+        localizacion = latitud_longitud;
+    }
+    public String getlocalizacion(){
+        return localizacion;
+    }
+    
+    public void setProvincia(Provincia prov){
+        provincia = prov;
+    }
+    public Provincia getProvincia(){
+        return provincia;
+    }
+    
+    public void setmaritimo(boolean valor){
+        maritimo = valor;
+    }
+    
+    public boolean getmaritimo(){
+        return maritimo;
+    }
+    
+    public void setDescripcion(String description){
+        descripcion =  description;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+    
+    @Override
+    public String toString(){
+        
+        return "El nombre del sismo es: ";
+    }
+}
